HaleBob/freetz

commit 697cf51276b611dee750bfb823bc8bc08a7d955f

classpath:  * bump version to 0.98  * allow any Java compiler to be used for bootstrapping, tested with eclipse and sun (now oracle) compilers  * move menuconfig entry  * add to external
git-svn-id: file:///var/svn/freetz/trunk@6321 149334a1-2f27-0410-a3b9-fc62619ac1e6

make/classpath/classpath.mk
@@ -1,13 +1,12 @@
-$(call PKG_INIT_BIN, 0.97.2)
+$(call PKG_INIT_BIN, 0.98)
 $(PKG)_LIB_VERSION:=0.0.0
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
-$(PKG)_SOURCE_MD5:=6a35347901ace03c31cc49751b338f31
+$(PKG)_SOURCE_MD5:=90c6571b8b0309e372faa0f9f6255ea9
 $(PKG)_SITE:=@GNU/$(pkg)
 
 $(PKG)_BINARY:=$($(PKG)_DIR)/lib/mini.jar
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/share/classpath/mini.jar
 
-#TODO: FREETZ_LIBRARY_PATH !?
 $(PKG)_LIBRARY_DIR:=/usr/lib/classpath
 $(PKG)_LIBNAMES_SHORT:=io lang langmanagement langreflect net nio util
 $(PKG)_LIBNAMES_LONG:=$(CLASSPATH_LIBNAMES_SHORT:%=libjava%.so.$($(PKG)_LIB_VERSION))
@@ -16,22 +15,26 @@ $(PKG)_LIBS_BUILD_DIR:=$(join $(CLASSPATH_LIBS_BUILD_DIR_SHORT:%=$($(PKG)_DIR)/n
 $(PKG)_LIBS_STAGING_DIR:=$(CLASSPATH_LIBNAMES_LONG:%=$(TARGET_TOOLCHAIN_STAGING_DIR)$(CLASSPATH_LIBRARY_DIR)/%)
 $(PKG)_LIBS_TARGET_DIR:=$(CLASSPATH_LIBNAMES_LONG:%=$($(PKG)_DEST_DIR)$(CLASSPATH_LIBRARY_DIR)/%)
 
-$(PKG)_BUILD_PREREQ += fastjar ecj
-$(PKG)_BUILD_PREREQ_HINT := Hint: on Debian-like systems this binaries are provided by packages of the same name
+$(PKG)_BUILD_PREREQ += fastjar javac
+$(PKG)_BUILD_PREREQ_HINT := Hint: on Debian-like systems fastjar is provided by the package of the same name, whereas javac (Java compiler) is provided by many packages. You could use either of them interchangeably.
 
 ifeq ($(strip $(FREETZ_TARGET_UCLIBC_VERSION_0_9_28)),y)
 $(PKG)_DEPENDS_ON := libiconv
 endif
 
 $(PKG)_CONFIGURE_ENV += HAVE_INET6_I_KNOW_IT_BETTER=$(if $(FREETZ_TARGET_IPV6_SUPPORT),yes,no)
+$(PKG)_CONFIGURE_ENV += JAVAC=javac
 
-$(PKG)_CONFIGURE_OPTIONS += --disable-gtk-peer
-$(PKG)_CONFIGURE_OPTIONS += --disable-qt-peer
+$(PKG)_CONFIGURE_OPTIONS += --disable-alsa
 $(PKG)_CONFIGURE_OPTIONS += --disable-gconf-peer
-$(PKG)_CONFIGURE_OPTIONS += --without-libiconv-prefix
+$(PKG)_CONFIGURE_OPTIONS += --disable-gjdoc
+$(PKG)_CONFIGURE_OPTIONS += --disable-gtk-peer
+$(PKG)_CONFIGURE_OPTIONS += --disable-gmp
+$(PKG)_CONFIGURE_OPTIONS += --disable-examples
 $(PKG)_CONFIGURE_OPTIONS += --disable-plugin
-$(PKG)_CONFIGURE_OPTIONS += --with-ecj
-$(PKG)_CONFIGURE_OPTIONS += --disable-Werror
+$(PKG)_CONFIGURE_OPTIONS += --disable-qt-peer
+$(PKG)_CONFIGURE_OPTIONS += --disable-rpath
+$(PKG)_CONFIGURE_OPTIONS += --without-libiconv-prefix
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)

make/classpath/Config.in
@@ -1,10 +1,8 @@
 config FREETZ_PACKAGE_CLASSPATH
-	bool "GNU classpath 0.97.2 (binary only)"
+	bool "GNU classpath 0.98 (binary only)"
 	default n
-	requires FREETZ_PACKAGE_JAMVM
 	select FREETZ_LIB_libiconv if FREETZ_TARGET_UCLIBC_VERSION_0_9_28
 	help
 		GNU Classpath, Essential Libraries for Java, is a GNU project to create
 		free core class libraries for use with virtual machines and compilers
-		for the java programming language. Classpath is still a work in
-		progress. The first public release will be version 1.0.
+		for the java programming language.

make/classpath/external.files
@@ -1,0 +1,6 @@
+if [ "$EXTERNAL_FREETZ_PACKAGE_CLASSPATH" == "y" ]; then
+	for lib in io langmanagement langreflect lang net nio util; do
+		EXTERNAL_FILES+=" /usr/lib/classpath/libjava${lib}.so.0.0.0"
+	done
+	EXTERNAL_FILES+=" /usr/share/classpath/mini.jar"
+fi

make/classpath/external.in
@@ -1,0 +1,14 @@
+config EXTERNAL_FREETZ_PACKAGE_CLASSPATH
+	depends on EXTERNAL_ENABLED && FREETZ_PACKAGE_CLASSPATH
+	bool "classpath"
+	default n
+	help
+		externals these file(s):
+		 /usr/lib/classpath/libjavaio.so.0.0.0
+		 /usr/lib/classpath/libjavalangmanagement.so.0.0.0
+		 /usr/lib/classpath/libjavalangreflect.so.0.0.0
+		 /usr/lib/classpath/libjavalang.so.0.0.0
+		 /usr/lib/classpath/libjavanet.so.0.0.0
+		 /usr/lib/classpath/libjavanio.so.0.0.0
+		 /usr/lib/classpath/libjavautil.so.0.0.0
+		 /usr/share/classpath/mini.jar

make/classpath/patches/100-incomplete_ipv6_test.patch
@@ -1,6 +1,6 @@
 --- configure
 +++ configure
-@@ -23810,12 +23810,17 @@
+@@ -24300,12 +24300,17 @@
  	 test ! -s conftest.err
         } && test -s conftest.$ac_objext; then
  
@@ -13,10 +13,10 @@
 +have_inet6_result="no (overridden)"
 +fi
  
--                     { echo "$as_me:$LINENO: result: yes" >&5
--echo "${ECHO_T}yes" >&6; }
-+                     { echo "$as_me:$LINENO: result: $have_inet6_result" >&5
-+echo "${ECHO_T}$have_inet6_result" >&6; }
+-                     { $as_echo "$as_me:$LINENO: result: yes" >&5
+-$as_echo "yes" >&6; }
++                     { $as_echo "$as_me:$LINENO: result: $have_inet6_result" >&5
++$as_echo "$have_inet6_result" >&6; }
  else
-   echo "$as_me: failed program was:" >&5
+   $as_echo "$as_me: failed program was:" >&5
  sed 's/^/| /' conftest.$ac_ext >&5

make/external.in
@@ -5,6 +5,7 @@ source make/bip/external.in
 source make/bittwist/external.in
 source make/bluez-utils/external.in
 source make/cifsmount/external.in
+source make/classpath/external.in
 source make/comgt/external.in
 source make/ctorrent/external.in
 source make/curl/external.in

make/jamvm/Config.in
@@ -27,6 +27,3 @@ config FREETZ_PACKAGE_JAMVM
 
 		Note, JamVM will not work with the class library from Sun or IBM's
 		Virtual Machines.
-
-# Include here a cleaner menu structure
-source make/classpath/Config.in

commit 21ac560ece5aae3c8caf9b6c379854ae68e19aa6

classpath:  * fix build problem introduced in r6321
git-svn-id: file:///var/svn/freetz/trunk@6329 149334a1-2f27-0410-a3b9-fc62619ac1e6

make/classpath/classpath.mk
@@ -35,6 +35,7 @@ $(PKG)_CONFIGURE_OPTIONS += --disable-plugin
 $(PKG)_CONFIGURE_OPTIONS += --disable-qt-peer
 $(PKG)_CONFIGURE_OPTIONS += --disable-rpath
 $(PKG)_CONFIGURE_OPTIONS += --without-libiconv-prefix
+$(PKG)_CONFIGURE_OPTIONS += --disable-Werror
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)