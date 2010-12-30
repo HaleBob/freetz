HaleBob/freetz

commit 843b9e123e914bc579d6ee352d486f7a93912f00

minicom:  * fix it doesn't compiled with uClibc-0.9.28  * add missing ncurses dependency
git-svn-id: file:///var/svn/freetz/trunk@6398 149334a1-2f27-0410-a3b9-fc62619ac1e6

make/minicom/minicom.mk
@@ -1,9 +1,15 @@
 $(call PKG_INIT_BIN,2.4)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
 $(PKG)_SITE:=http://alioth.debian.org/frs/download.php/3195
+$(PKG)_SOURCE_MD5:=700976a3c2dcc8bbd50ab9bb1c08837b
+
 $(PKG)_BINARY:=$($(PKG)_DIR)/src/minicom
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/minicom
-$(PKG)_SOURCE_MD5:=700976a3c2dcc8bbd50ab9bb1c08837b
+
+$(PKG)_DEPENDS_ON += ncurses
+ifeq ($(strip $(FREETZ_TARGET_UCLIBC_VERSION_0_9_28)),y)
+$(PKG)_DEPENDS_ON += libiconv
+endif
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)

make/minicom/Config.in
@@ -1,8 +1,8 @@
 config FREETZ_PACKAGE_MINICOM
 	bool "minicom 2.4 (binary only)"
 	select FREETZ_LIB_libncurses
+	select FREETZ_LIB_libiconv if FREETZ_TARGET_UCLIBC_VERSION_0_9_28
 	default n
 	help
 		Minicom is a menu driven communications program. It emulates ANSI and
 		VT102 terminals. It has a dialing directory and auto zmodem download.
-

make/minicom/patches/100-missing_LTLIBICONV.patch
@@ -1,0 +1,11 @@
+--- src/Makefile.in
++++ src/Makefile.in
+@@ -328,7 +328,7 @@
+ 	$(LINK) $(ascii_xfr_OBJECTS) $(ascii_xfr_LDADD) $(LIBS)
+ minicom$(EXEEXT): $(minicom_OBJECTS) $(minicom_DEPENDENCIES) 
+ 	@rm -f minicom$(EXEEXT)
+-	$(LINK) $(minicom_OBJECTS) $(minicom_LDADD) $(LIBS)
++	$(LINK) $(minicom_OBJECTS) $(minicom_LDADD) $(LIBS) $(LTLIBICONV)
+ minicom.keyserv$(EXEEXT): $(minicom_keyserv_OBJECTS) $(minicom_keyserv_DEPENDENCIES) 
+ 	@rm -f minicom.keyserv$(EXEEXT)
+ 	$(LINK) $(minicom_keyserv_OBJECTS) $(minicom_keyserv_LDADD) $(LIBS)

commit 06691b21bbea6f97432ea681d94ddc9a7b263094

minicom:  * fix compilations problems with uClibc versions >= 0.9.29 introduced in r6398
git-svn-id: file:///var/svn/freetz/trunk@6402 149334a1-2f27-0410-a3b9-fc62619ac1e6

make/minicom/minicom.mk
@@ -17,6 +17,7 @@ $(PKG_CONFIGURED_CONFIGURE)
 
 $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
 	$(SUBMAKE) -C $(MINICOM_DIR) \
+		ICONVLIB="$(if $(FREETZ_TARGET_UCLIBC_VERSION_0_9_28),-liconv)" \
 		AM_CFLAGS=""
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)

make/minicom/patches/100-iconv_workaround.patch
@@ -5,7 +5,7 @@
  minicom$(EXEEXT): $(minicom_OBJECTS) $(minicom_DEPENDENCIES) 
  	@rm -f minicom$(EXEEXT)
 -	$(LINK) $(minicom_OBJECTS) $(minicom_LDADD) $(LIBS)
-+	$(LINK) $(minicom_OBJECTS) $(minicom_LDADD) $(LIBS) $(LTLIBICONV)
++	$(LINK) $(minicom_OBJECTS) $(minicom_LDADD) $(LIBS) $(ICONVLIB)
  minicom.keyserv$(EXEEXT): $(minicom_keyserv_OBJECTS) $(minicom_keyserv_DEPENDENCIES) 
  	@rm -f minicom.keyserv$(EXEEXT)
  	$(LINK) $(minicom_keyserv_OBJECTS) $(minicom_keyserv_LDADD) $(LIBS)