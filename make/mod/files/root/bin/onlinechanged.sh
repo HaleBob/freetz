HaleBob/freetz

commit f9760e6152799aec9c641aff7856f08e1976a00d

Workaround for hanging `onlinechanged.sh` (AVM problem): kill old sibling scripts if the semaphore `/var/run/onlinechanged` is older than 3 minutes. Closes #1603.
git-svn-id: file:///var/svn/freetz/trunk@8325 149334a1-2f27-0410-a3b9-fc62619ac1e6

make/mod/files/root/bin/onlinechanged.sh
@@ -14,6 +14,15 @@ log() {
 	logger -t ONLINECHANGED[$$] "[$OC_STATE] $*"
 }
 
+# semaphore older than 3 min -> kill waiting sibling scripts
+if [ "$(find $PID_FILE -prune -mmin +3 2>/dev/null)" == "$PID_FILE" ]; then
+	for pid in $(pidof onlinechanged.sh | sed "s/ \?$$//"); do
+		log "killing old process #$pid"
+		kill $pid
+	done
+	rm -rf $PID_FILE 2>/dev/null
+fi
+
 # shutdown: do nothing
 if [ -e /var/run/shutdown ]; then
 	log "disabled"

commit 12a5301db15bf4497eeb958e5aa161e0e2b8de97

fix deadlock by timesync introduced in r8325 (refs #1603)
git-svn-id: file:///var/svn/freetz/trunk@8377 149334a1-2f27-0410-a3b9-fc62619ac1e6

make/mod/files/root/bin/onlinechanged.sh
@@ -14,8 +14,8 @@ log() {
 	logger -t ONLINECHANGED[$$] "[$OC_STATE] $*"
 }
 
-# semaphore older than 3 min -> kill waiting sibling scripts
-if [ "$(find $PID_FILE -prune -mmin +3 2>/dev/null)" == "$PID_FILE" ]; then
+# semaphore older than 3 min -> kill waiting sibling scripts (not during startup)
+if [ -e /tmp/.modstarted -a "$(find $PID_FILE -prune -mmin +3 2>/dev/null)" == "$PID_FILE" ]; then
 	for pid in $(pidof onlinechanged.sh | sed "s/ \?$$//"); do
 		log "killing old process #$pid"
 		kill $pid